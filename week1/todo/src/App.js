f62947296b9de58f3b4e4fd34a2ad1af2b77365e
jesse-hampton-bu <65149406+jesse-hampton-bu@users.noreply.github.com>
2021-02-21 22:08:44 -0800

    week 1 commit

---
+++ week1/todo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+    return(
+        <div>
+            <p>800 Brand punch list</p>
+            <input type="checkbox" />Set Drop In Refers.<br/>
+            <input type="checkbox" />Peel Laser Film<br/>
+            <input type="checkbox" />Remove Sharpie And Pencil Marks<br/>
+            <input type="checkbox" />Silicone<br/>
+        </div>
+    )
+}
+
+export default App
